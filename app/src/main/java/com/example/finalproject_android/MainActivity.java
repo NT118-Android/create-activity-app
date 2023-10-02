3e70934dc8daaf78db1ae66f75b225e328ecf5c2
longle1 <ltphilong2001@gmail.com>
2023-10-02 22:02:41 +0700

    initial project

---
+++ app/src/main/java/com/example/finalproject_android/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.finalproject_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
